70f2a79616b741134f360b83e7bc5db9621a9538
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-25 10:35:02 +0330

    add database postgresql in project test

--- cbv/settings.py
+++ cbv/settings.py
@@ -30,6 +30,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+REST_FRAMEWORKS = [
+    'rest_framework',
+]
+
+DB = [
+    'django.contrib.postgres',
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,7 +48,8 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'home.apps.HomeConfig',
     'courses.apps.CoursesConfig',
-    'rest_framework'
+    *REST_FRAMEWORKS,
+    *DB,
 ]
 
 MIDDLEWARE = [
@@ -80,13 +89,23 @@ WSGI_APPLICATION = 'cbv.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_test',
+        'USER': 'django_test',
+        'PASSWORD': 'django_test.1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
